1cc6b1c807c8e2bacf5ec4b83fedfca9fe1542d3
nathan hains <nathanhains14@yahoo.com>
2021-05-13 21:50:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 13
+let num4 = 9
+let mod = num3%num4
+
+
+let max = Math.max(0,1,20)
